a5e2b41c89fdced8188eac7cec4d3d58e434cb1b
Andrew Brunton <andrew@Andrews-MacBook-Pro.local>
2021-05-16 20:58:11 +1000

    added sudo to all deploy commands

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ git pull
 
 # stop docker container
 echo "Stopping container"
-docker stop $(docker ps | grep 'python-docker' | awk '{ print $1 }')
+sudo docker stop $(docker ps | grep 'python-docker' | awk '{ print $1 }')
 
 # Build the image
-docker build --tag python-docker .
+sudo docker build --tag python-docker .
 
 # start container
 echo "Starting container"
-docker run --rm -d -p 5000:5000 python-docker
+sudo docker run --rm -d -p 5000:5000 python-docker
